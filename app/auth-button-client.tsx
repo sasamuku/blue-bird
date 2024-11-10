53413b663456ff64ad77a581728f2ba5d4e0bc09
sasamuku <sasazawa3330@gmail.com>
2024-11-10 14:38:27 +0900

    implement authentication

---
+++ app/auth-button-client.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { createClientComponentClient, Session } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
+export default function AuthButtonClient({ session }: { session: Session | null }) {
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options : {
+        redirectTo: "http://localhost:3000/auth/callback"
+      }
+    });
+  };
+
+  return session ? (
+    <button onClick={handleSignOut}>Logout</button>
+  ) : (
+    <button onClick={handleSignIn}>Login</button>
+  )
+}
