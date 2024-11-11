0f1b98543cf151eccea97e71ea0505c3d51cb865
sasamuku <sasazawa3330@gmail.com>
2024-11-11 09:10:26 +0900

    implement likes

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import AuthButtonServer from "./auth-button-server";
 import NewTweet from "./new-tweet";
+import Likes from "./likes";
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -13,9 +14,15 @@ export default async function Home() {
     redirect("/login");
   }
 
-  const { data: tweets } = await supabase
+  const { data } = await supabase
     .from("tweets")
-    .select("*, profiles(*)");
+    .select("*, profiles(*), likes(*)");
+
+  const tweets = data?.map(tweet => ({
+    ...tweet,
+    user_has_liked_tweet: tweet.likes.find(like => like.user_id === session.user.id),
+    likes: tweet.likes.length
+  })) ?? [];
 
   return (
     <>
@@ -30,6 +37,7 @@ export default async function Home() {
             <p>
               {tweet?.title}
             </p>
+            <Likes tweet={tweet} />
           </div>
         ))
       }
