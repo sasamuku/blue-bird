92a2e5adf6b81a7e6cf29c5896a763d2262f3a0f
sasamuku <sasazawa3330@gmail.com>
2024-11-13 09:13:07 +0900

    implement dynamic update

--- app/tweets.tsx
+++ app/tweets.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { use, useEffect } from "react";
 import Likes from "./likes";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 // import { useOptimistic } from "react";
 
 export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
@@ -19,6 +22,26 @@ export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
   // });
 
   const addOptimisticTweet = () => {};
+
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  useEffect(() => {
+    const channel = supabase.channel("realtime tweets").on
+    (
+      "postgres_changes", {
+        event: "*",
+        schema: "public",
+        table: "tweets",
+      }, (payload) => {
+        router.refresh();
+      }
+    ).subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, router]);
 
   return tweets.map((tweet) => (
     <div key={tweet.id}>
